36c508caf9c6fba94d9c9822b91af94fc5eeb39c
rdewit91 <r.dewit91@outlook.com>
2024-04-29 11:34:30 -0400

    mreLst last push

--- dataStrucs/mreLst.py
+++ dataStrucs/mreLst.py
@@ -302,3 +302,127 @@ a = {x for x in 'abracadabra' if x not in 'abc'}
 print(a)
 
 #           ---         Dictionaries            ---
+
+#Another useful data type built into Python is the dictionary (see Mapping Types — dict). Dictionaries are sometimes found in other languages as “associative memories” or “associative arrays”. Unlike sequences, which are indexed by a range of numbers, dictionaries are indexed by keys, which can be any immutable type; strings and numbers can always be keys. Tuples can be used as keys if they contain only strings, numbers, or tuples; if a tuple contains any mutable object either directly or indirectly, it cannot be used as a key. You can’t use lists as keys, since lists can be modified in place using index assignments, slice assignments, or methods like append() and extend().
+
+#It is best to think of a dictionary as a set of key: value pairs, with the requirement that the keys are unique (within one dictionary). A pair of braces creates an empty dictionary: {}. Placing a comma-separated list of key:value pairs within the braces adds initial key:value pairs to the dictionary; this is also the way dictionaries are written on output.
+
+#The main operations on a dictionary are storing a value with some key and extracting the value given the key. It is also possible to delete a key:value pair with del. If you store using a key that is already in use, the old value associated with that key is forgotten. It is an error to extract a value using a non-existent key.
+
+#Performing list(d) on a dictionary returns a list of all the keys used in the dictionary, in insertion order (if you want it sorted, just use sorted(d) instead). To check whether a single key is in the dictionary, use the in keyword.
+
+#Here is a small example using a dictionary:
+
+tel = {'jack' : 4098, 'sape' : 4139}
+
+tel['guido'] = 4127
+print(tel)
+
+print(tel['jack'])
+
+del tel['sape']
+tel['irv'] = 4127
+print(tel)
+
+print(list(tel))
+
+print(sorted(tel))
+
+print('guido' in tel)
+print('jack' not in tel)
+
+#The dict() constructor builds dictionaries directly from sequences of key-value pairs:
+
+dict1 = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+print(dict1)
+
+#In addition, dict comprehensions can be used to create dictionaries from arbitrary key and value expressions:
+
+dict2 = {x: x**2 for x in (2, 4, 6)}
+print(dict2)
+
+#When the keys are simple strings, it is sometimes easier to specify pairs using keyword arguments:
+
+dict3 = dict(sape=4139, guido=4127, jack=4098)
+print(dict3)
+
+#           ---         Looping Techniques          ---
+
+# When looping through dictionaries, the key and corresponding value can be retrieved at the same time using the items() method.
+
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in knights.items():
+    print(k, v)
+    
+# When looping through a sequence, the position index and corresponding value can be retrieved at the same time using the enumerate() function.
+
+for i, v in enumerate(['tic', 'tac', 'toe']):
+    print(i, v)
+    
+# To loop over two or more sequences at the same time, the entries can be paired with the zip() function.
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('what is your {0}? it is {1}.' .format(q, a))
+    
+#To loop over a sequence in reverse, first specify the sequence in a forward direction and then call the reversed() function.
+
+for i in reversed(range(1, 10, 2)):
+    print(i)
+    
+#To loop over a sequence in sorted order, use the sorted() function which returns a new sorted list while leaving the source unaltered.
+
+bascket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for i in sorted(basket):
+    print(i)
+    
+# Using set() on a sequence eliminates duplicate elements. The use of sorted() in combination with set() over a sequence is an idiomatic way to loop over unique elements of the sequence in sorted order.
+
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for f in sorted(set(basket)):
+    print(f)
+    
+#It is sometimes tempting to change a list while you are looping over it; however, it is often simpler and safer to create a new list instead.
+
+import math
+raw_data = [56.2, float('NaN'), 51.7, 55.3, 52.5, float('NaN'), 47.8]
+filtered_data = []
+for value in raw_data:
+    if not math.isnan(value):
+        filtered_data.append(value)
+print(filtered_data)
+
+#           ---         More on Conditions          ---
+
+#The conditions used in while and if statements can contain any operators, not just comparisons.
+
+#The comparison operators in and not in are membership tests that determine whether a value is in (or not in) a container. The operators is and is not compare whether two objects are really the same object. All comparison operators have the same priority, which is lower than that of all numerical operators.
+
+#Comparisons can be chained. For example, a < b == c tests whether a is less than b and moreover b equals c.
+
+#Comparisons may be combined using the Boolean operators and and or, and the outcome of a comparison (or of any other Boolean expression) may be negated with not. These have lower priorities than comparison operators; between them, not has the highest priority and or the lowest, so that A and not B or C is equivalent to (A and (not B)) or C. As always, parentheses can be used to express the desired composition.
+
+#The Boolean operators and and or are so-called short-circuit operators: their arguments are evaluated from left to right, and evaluation stops as soon as the outcome is determined. For example, if A and C are true but B is false, A and B and C does not evaluate the expression C. When used as a general value and not as a Boolean, the return value of a short-circuit operator is the last evaluated argument.
+
+#It is possible to assign the result of a comparison or other Boolean expression to a variable. For example,
+
+string1, string2, string3 = '', 'Tronfheim', 'Hammer Dance'
+non_null = string1 or string2 or string3
+print(non_null)
+
+# Note that in Python, unlike C, assignment inside expressions must be done explicitly with the walrus operator :=. This avoids a common class of problems encountered in C programs: typing = in an expression when == was intended.
+
+#           ---         Comparing Sequences and Other Types         ---
+
+#Sequence objects typically may be compared to other objects with the same sequence type. The comparison uses lexicographical ordering: first the first two items are compared, and if they differ this determines the outcome of the comparison; if they are equal, the next two items are compared, and so on, until either sequence is exhausted. If two items to be compared are themselves sequences of the same type, the lexicographical comparison is carried out recursively. If all items of two sequences compare equal, the sequences are considered equal. If one sequence is an initial sub-sequence of the other, the shorter sequence is the smaller (lesser) one. Lexicographical ordering for strings uses the Unicode code point number to order individual characters. Some examples of comparisons between sequences of the same type:
+
+(1, 2, 3)              < (1, 2, 4)
+[1, 2, 3]              < [1, 2, 4]
+'ABC' < 'C' < 'Pascal' < 'Python'
+(1, 2, 3, 4)           < (1, 2, 4)
+(1, 2)                 < (1, 2, -1)
+(1, 2, 3)             == (1.0, 2.0, 3.0)
+(1, 2, ('aa', 'ab'))   < (1, 2, ('abc', 'a'), 4)
+
+#Note that comparing objects of different types with < or > is legal provided that the objects have appropriate comparison methods. For example, mixed numeric types are compared according to their numeric value, so 0 equals 0.0, etc. Otherwise, rather than providing an arbitrary ordering, the interpreter will raise a TypeError exception.
